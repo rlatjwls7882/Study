dcbb5fff0a04aa41309afd0c4bd6e4beec1204f7
rlatjwls7882 <107532530+rlatjwls7882@users.noreply.github.com>
2023-08-26 19:09:12 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+	int S,K,H;
+	scanf("%d%d%d",&S,&K,&H);
+	
+	if(S+K+H>=100) printf("OK");
+	else if(S<K&&S<H) printf("Soongsil");
+	else if(K<S&&K<H) printf("Korea");
+	else printf("Hanyang");
+	
+	return 0;
+}
