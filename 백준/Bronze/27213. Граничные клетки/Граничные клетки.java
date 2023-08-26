2e17680c4893740d078ae8efcd31966e89330d1e
rlatjwls7882 <107532530+rlatjwls7882@users.noreply.github.com>
2023-08-26 19:27:46 +0900

    [Bronze IV] Title: Граничные клетки, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/27213. Граничные клетки/Граничные клетки.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long m = Long.valueOf(bf.readLine());
+		long n = Long.valueOf(bf.readLine());
+		
+		if(m<3||n<3) bw.write(m*n+"");
+		else bw.write(m*n-(m-2)*(n-2)+"");
+		
+		bw.close();
+	} // end of main
+} // end of main class
