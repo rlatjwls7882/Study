41af7f244d42384bcd8a2177e1b501979ca121ba
rlatjwls7882 <107532530+rlatjwls7882@users.noreply.github.com>
2023-08-26 19:19:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 140 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long n = Long.valueOf(bf.readLine());
+		
+		bw.write(n*(n-1)/2+"\n2");
+		
+		bw.close();
+	} // end of main
+} // end of main class
