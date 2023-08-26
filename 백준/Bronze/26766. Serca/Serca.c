7261ff17511dfbd082efd740bdc17d9288ea61c2
rlatjwls7882 <107532530+rlatjwls7882@users.noreply.github.com>
2023-08-26 19:26:48 +0900

    [Bronze V] Title: Serca, Time: 132 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+	int N;
+	scanf("%d",&N);
+	
+	while(N--){
+		puts(" @@@   @@@ ");
+		puts("@   @ @   @");
+		puts("@    @    @");
+		puts("@         @");
+		puts(" @       @ ");
+		puts("  @     @  ");
+		puts("   @   @   ");
+		puts("    @ @    ");
+		puts("     @    ");
+	}
+	
+	return 0;
+}
