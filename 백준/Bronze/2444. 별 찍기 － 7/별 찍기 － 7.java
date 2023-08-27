65ba08550f4826d067bcbd648447d5cfa1c5e302
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-08-27 15:05:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 140 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	// N 입력
+    	int N = Integer.valueOf(br.readLine());
+    	
+    	// 별 출력
+    	for(int i=1;i<=N;i++) {
+    		
+    		for(int j=1;j<=N-i;j++)
+    			bw.write(" ");
+    		
+    		for(int j=2*i-1;j>0;j--)
+    			bw.write("*");
+    		
+    		bw.write("\n");
+    	}
+    	
+    	for(int i=N-1;i>=0;i--) {
+    		
+    		for(int j=1;j<=N-i;j++)
+    			bw.write(" ");
+    		
+    		for(int j=2*i-1;j>0;j--)
+    			bw.write("*");
+    		
+    		bw.write("\n");
+    	}
+    	
+    	bw.close();
+    } // end of main
+} // end of Main class
