48e32ca113e2f7184744bf3615af2ea946caae48
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-09-02 20:16:58 +0900

    Update 숫자 카드 2.java

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,7 +6,6 @@ import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
