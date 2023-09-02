9c024936b435c72599cb1092dbcce2c2e4837734
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-09-02 16:39:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1924 ms, Memory: 194992 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        // N 입력
+        int N = Integer.valueOf(br.readLine());
+        
+        // N개의 정수 오름차순으로 정렬
+        TreeSet<Integer> sort = new TreeSet<Integer>();
+        
+        for(int i=0;i<N;i++)
+        	sort.add(Integer.valueOf(br.readLine()));
+        
+        // 오름차순으로 출력
+        Iterator<Integer> it = sort.iterator();
+        
+        for(int i=0;i<N;i++)
+        	bw.write(it.next()+"\n");
+        
+        bw.close();
+    } // end of main
+} // end of Main class
