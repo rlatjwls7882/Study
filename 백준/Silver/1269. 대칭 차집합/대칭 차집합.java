cdc0b167e383fa1c4855a6349ecc07a991bf5cb4
rlatjwls7882 <rlatjwls7882@gmail.com>
2023-09-03 21:19:56 +0900

    Update 대칭 차집합.java

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
