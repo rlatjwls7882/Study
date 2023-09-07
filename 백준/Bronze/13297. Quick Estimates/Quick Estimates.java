51eb3e64e6a0c68ec43a8fc0e156962d2e6a5163
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-09-08 02:27:17 +0900

    [Bronze III] Title: Quick Estimates, Time: 144 ms, Memory: 15680 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 테스트 케이스의 수 T 입력
+        int T = Integer.valueOf(br.readLine());
+        
+        while(T--!=0) {
+        	
+        	// 비용의 단위 출력
+        	String cost = br.readLine();
+        	
+        	bw.write(cost.length()+"\n");
+        }
+        
+        bw.close();
+    } // end of main
+} // end of Main class
