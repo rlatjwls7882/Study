2e8049b3074b44f3ce2e9fd02ef0c384f65b38da
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-09-08 02:45:18 +0900

    [Bronze III] Title: What is n, Daddy?, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/6794. What is n， Daddy？/What is n， Daddy？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // N 입력
+        int N = Integer.valueOf(br.readLine());
+        
+        // 숫자를 세는 방법의 수 계산
+        int cnt=0;
+        
+        for(int i=0;i<=N;i++)
+        	if(N-i>=i&&N-i<=5)
+        		cnt++;
+        
+        // 숫자를 세는 방법의 수 출력
+        bw.write(cnt+"");
+        bw.close();
+    } // end of main
+} // end of Main class
