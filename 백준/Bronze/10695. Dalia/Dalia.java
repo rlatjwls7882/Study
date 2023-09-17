a95a4b16ef4bde4b66d78ec7744f95b3840b3a4a
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-09-17 13:02:57 +0900

    [Bronze III] Title: Dalia, Time: 144 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/10695. Dalia/Dalia.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        // 테스트 케이스의 수 T 입력
+        int T = Integer.valueOf(br.readLine());
+        
+        for(int i=1;i<=T;i++) {
+        	
+        	// 체스판의 길이 n, 시작지점(r1, c1), 도착지점(r2, c2) 입력
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int n = Integer.valueOf(st.nextToken());
+        	int r1 = Integer.valueOf(st.nextToken());
+        	int c1 = Integer.valueOf(st.nextToken());
+        	int r2 = Integer.valueOf(st.nextToken());
+        	int c2 = Integer.valueOf(st.nextToken());
+        	
+        	// 말이 시작시점에서 단 한번의 움직임으로 도착지점에 도달할 수 있는지 출력
+        	if((Math.abs(r1-r2)==1&&Math.abs(c1-c2)==2)
+               ||(Math.abs(r1-r2)==2&&Math.abs(c1-c2)==1))
+        		bw.write(String.format("Case %d: YES\n", i));
+        	else
+        		bw.write(String.format("Case %d: NO\n", i));
+        }
+        
+        bw.close();
+    } // end of main
+} // end of Main class
