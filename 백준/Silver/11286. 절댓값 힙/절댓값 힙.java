eb8d5d16010c36ecc77027083647b9f7036cdaf7
rlatjwls7882 <rlatjwls7882@gmail.com>
2023-10-08 14:14:39 +0900

    Update 절댓값 힙.java

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,10 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,30 +15,31 @@ public class Main {
     	int N = Integer.valueOf(br.readLine());
     	
     	// N개의 연산 수행
-    	PriorityQueue<Long> minus = new PriorityQueue<>(Collections.reverseOrder());
-    	PriorityQueue<Long> plus = new PriorityQueue<>();
+    	PriorityQueue<Long> absMin = new PriorityQueue<>(new Comparator<Long>() {
+
+    		@Override
+    		public int compare(Long o1, Long o2) {
+
+    			if(Math.abs(o1)>Math.abs(o2))
+    				return 1;
+    			else if(Math.abs(o1)==Math.abs(o2)&&o1>o2)
+    				return 1;
+    			else
+    				return -1;
+    		}
+    	});
     	
     	while(N-->0) {
     		
     		long x = Long.valueOf(br.readLine());
     		
     		// x가 0이 아닌 경우
-    		if(x>0)
-    			plus.add(x);
-    		else if(x<0)
-    			minus.add(x);
+    		if(x!=0)
+    			absMin.add(x);
     		
     		// x가 0인 경우
-    		else if(!minus.isEmpty()&&!plus.isEmpty()) {
-    			if(-minus.peek()<=plus.peek())
-    				bw.write(minus.poll()+"\n");
-    			else
-    				bw.write(plus.poll()+"\n");
-    		}
-    		else if(!minus.isEmpty()&&plus.isEmpty())
-    			bw.write(minus.poll()+"\n");
-    		else if(minus.isEmpty()&&!plus.isEmpty())
-    			bw.write(plus.poll()+"\n");
+    		else if(!absMin.isEmpty())
+    			bw.write(absMin.poll()+"\n");
     		else
     			bw.write("0\n");
     	}
