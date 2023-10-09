7a69d316fa85704ece1a453916502b7dc017a4ee
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-10-09 22:14:23 +0900

    [Bronze III] Title: Hurricane Warning, Time: 352 ms, Memory: 39260 KB -BaekjoonHub

---
+++ 백준/Bronze/30266. Hurricane Warning/Hurricane Warning.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	// 데이터 세트의 수 K
+    	int K = Integer.valueOf(br.readLine());
+    	
+    	for(int numCases=1;numCases<=K;numCases++) {
+    		
+    		// 사람의 수 n, 경보를 보낼 수 있는 미디어의 종류 m
+    		int n = Integer.valueOf(br.readLine());
+    		String m = br.readLine();
+    		
+    		// 허리케인 경보를 받는 사람의 수 계산
+    		int cnt=0;
+    		
+    		while(n-->0) {
+    			
+    			String receive = br.readLine();
+    			
+    			canSend:
+    			for(int i=0;i<m.length();i++)
+    				for(int j=0;j<receive.length();j++)
+    					if(m.charAt(i)==receive.charAt(j)) {
+    						cnt++;
+    						break canSend;
+    					}
+    		}
+    		
+    		bw.write(String.format("Data Set %d:\n%d\n\n", numCases, cnt));
+    	}
+    	
+    	bw.close();
+    } // end of main
+} // end of Main class
