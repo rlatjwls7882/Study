ce221e1aef529ec6039cd388819d14b4b13c1eea
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-10-11 11:22:06 +0900

    [Silver II] Title: 베르트랑 공준, Time: 212 ms, Memory: 18308 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -9,6 +9,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     	
+    	// 에라토스테네스의 체 사용
+    	int length = 123456*2+1;
+    	boolean[] notSosu = new boolean[length];
+    	notSosu[0]=notSosu[1]=true;
+    	
+    	for(int i=2;i*i<=length;i++)
+    		if(!notSosu[i])
+    			for(int j=i*i;j<=length;j+=i)
+    				notSosu[j]=true;
+    	
     	while(true) {
     		
     		// n 입력
@@ -18,17 +28,9 @@ public class Main {
     		// n<i<=2n 사이의 소수 i의 개수 계산
     		int sosu=0;
     		
-    		for(int i=n+1;i<=2*n;i++) {
-    			boolean isSosu=true;
-    			
-    			for(int j=2;j*j<=i;j++)
-    				if(i%j==0) {
-    					isSosu=false;
-    					break;
-    				}
-    			
-    			if(isSosu) sosu++;
-    		}
+    		for(int i=n+1;i<=2*n;i++)
+    			if(!notSosu[i])
+    				sosu++;
     		
     		bw.write(sosu+"\n");
     	}
