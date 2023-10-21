ad6fd90d95feec467001032ef9c6bdf24d4f6e8b
rlatjwls7882 <rlatjwls7882@gmail.com>
2023-10-22 00:50:56 +0900

    Update 막대기.java

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -10,15 +10,9 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		// 막대는 2^0~2^6의 중복되지 않은 합으로 이루어짐(=이진수)
-		String X = Integer.toBinaryString(Integer.valueOf(br.readLine()));
-		
 		// 이진수로 나타냈을 때 1의 개수(막대를 풀로 붙이는 횟수) 계산
-		int sumOfOne=0;
-		for(int i=0;i<X.length();i++)
-			if(X.charAt(i)=='1')
-				sumOfOne++;
-		
-		bw.write(sumOfOne+"");
+		int X = Integer.valueOf(br.readLine());
+		bw.write(Integer.bitCount(X)+"");
 		
 		bw.close();
 	} // end of main
