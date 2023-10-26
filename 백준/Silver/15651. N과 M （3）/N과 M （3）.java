d9e31f3caa61785b17eb5cf37d40439c40f0eaa8
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-10-26 21:00:53 +0900

    [Silver III] Title: N과 M (3), Time: 576 ms, Memory: 158360 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int N;
+	static int M;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+
+		// 자연수의 개수 N, 고르는 개수 M
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.valueOf(st.nextToken());
+		M = Integer.valueOf(st.nextToken());
+		
+		// 방문한 노드를 담을 배열 arr
+		arr = new int[M];
+		
+		// 1부터 N까지 M개를 고른 수열 출력
+		dfs(0);
+		
+		bw.close();
+	} // end of main()
+	
+	static void dfs(int length) throws IOException {
+		
+		// 길이가 M인 경우 출력
+		if(length==M) {
+			for(int i:arr)
+				bw.write(i+" ");
+			bw.write("\n");
+			
+			return;
+		}
+		
+		// 길이가 M이 될때까지 방문
+		for(int nodeNum=1;nodeNum<=N;nodeNum++) {
+			arr[length]=nodeNum;
+			dfs(length+1);
+		}
+	} // end of dfs()
+} // end of Main class
