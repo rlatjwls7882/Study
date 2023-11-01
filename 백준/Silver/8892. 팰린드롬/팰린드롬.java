b95aa3e6e3d08ba442db621ba1cfdd52d2228516
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-01 21:08:53 +0900

    [Silver V] Title: 팰린드롬, Time: 304 ms, Memory: 35480 KB -BaekjoonHub

---
+++ 백준/Silver/8892. 팰린드롬/팰린드롬.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.valueOf(br.readLine());
+		
+		while(T-->0) {
+			
+			// 문자열 N개 입력
+			int N = Integer.valueOf(br.readLine());
+			String[] strings = new String[N];
+			for(int i=0;i<N;i++)
+				strings[i] = br.readLine();
+			
+			// 2개의 문자열을 골라 팰린드롬을 만들 수 있는지 확인
+			boolean isPalindrome = false;
+			
+			for(int i=0;i<N;i++)
+				for(int j=0;j<N;j++)
+					if(i!=j&&isPalindrome(strings[i], strings[j])) {
+						bw.write(strings[i]);
+						bw.write(strings[j]);
+						bw.newLine();
+						isPalindrome=true;
+						i=j=N;
+					}
+			
+			if(!isPalindrome) bw.write("0\n");
+		}
+		
+		bw.close();
+	} // end of main()
+	
+	static boolean isPalindrome(String s1, String s2) {
+		boolean isPalindrome=true;
+		
+		String string = s1+s2;
+		int length = string.length();
+		
+		for(int i=0;i<length/2;i++)
+			if(string.charAt(i)!=string.charAt(length-i-1)) {
+				isPalindrome=false;
+				break;
+			}
+		
+		return isPalindrome;
+	}
+	
+} // end of Main class
