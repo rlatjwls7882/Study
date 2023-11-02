4e4f3d1ec7477bb623c163976bad8be8167e9413
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-02 16:58:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 932 ms, Memory: 132612 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -15,29 +15,30 @@ public class Main {
 		int N = Integer.valueOf(st.nextToken());
 		int M = Integer.valueOf(st.nextToken());
 		
-		// 가로줄의 구간합 계산
-		int[][] sum = new int[N][N+1];
-		for(int i=0;i<N;i++) {
+		// 사각형 범위의 누적합 계산
+		int[][] sum = new int[N+1][N+1];
+		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			for(int j=1;j<=N;j++) {
-				sum[i][j] = sum[i][j-1] + Integer.valueOf(st.nextToken());
+				sum[i][j] = Integer.valueOf(st.nextToken());
+			}
+		}
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=N;j++) {
+				sum[i][j] += sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
 			}
 		}
 		
 		// (x1, y1)~(x2, y2)의 합 계산
 		while(M-->0) {
 			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.valueOf(st.nextToken())-1;
+			int x1 = Integer.valueOf(st.nextToken());
 			int y1 = Integer.valueOf(st.nextToken());
-			int x2 = Integer.valueOf(st.nextToken())-1;
+			int x2 = Integer.valueOf(st.nextToken());
 			int y2 = Integer.valueOf(st.nextToken());
 			
-			int result=0;
-			while(x1<=x2) {
-				result += sum[x1][y2]-sum[x1][y1-1];
-				x1++;
-			}
+			int result = sum[x2][y2]-sum[x1-1][y2]-sum[x2][y1-1]+sum[x1-1][y1-1];
 			
 			bw.write(Integer.toString(result));
 			bw.newLine();
