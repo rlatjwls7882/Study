dd0f15e6ca53eb936c50de4276ea44e833b578d7
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-02 13:54:33 +0900

    [Silver II] Title: 1차원 2048과 쿼리, Time: 780 ms, Memory: 101524 KB -BaekjoonHub

---
+++ 백준/Silver/27515. 1차원 2048과 쿼리/1차원 2048과 쿼리.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// 쿼리의 개수 Q
+		int Q = Integer.valueOf(br.readLine());
+		
+		// 쿼리 수행
+		long sum=0;
+		
+		while(Q-->0) {
+			sum += Long.valueOf(br.readLine());
+			
+			// 만들 수 있는 가장 큰 2의 거듭제곱 값(2진수로 나타냈을 때의 최상위 비트) 계산
+			bw.write(Long.highestOneBit(sum)+"\n");
+		}
+
+		bw.close();
+	} // end of main()
+} // end of Main class
