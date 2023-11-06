aa7a9e7afadf6b7a336681aaa1f8bcd4347d985b
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-06 13:14:53 +0900

    [Silver I] Title: 쿼드트리, Time: 136 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static int cntBlue, cntWhite;
+	static int[][] movie;
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 한 변의 길이가 N인 영상 입력
+		int N = Integer.valueOf(br.readLine());
+		
+		movie = new int[N][N];
+		for(int i=0;i<N;i++) {
+			String string = br.readLine();
+			
+			for(int j=0;j<N;j++) {
+				movie[i][j] = string.charAt(j)-'0';
+			}
+		}
+		
+		// 영상 압축 결과 출력
+		cntPaper(0, 0, N);
+		bw.write(sb.toString());
+		
+		bw.close();
+	} // end of main()
+	
+	static void cntPaper(int startRow, int startCol, int length) {
+		
+		
+		// 한 구역이 모두 같은 값인 경우
+		if(isSameVal(startRow, startCol, length)) {
+			sb.append(movie[startRow][startCol]);
+			return;
+		}
+
+		// 한 구역이 모두 같은 값이 아닌 경우
+		else {
+			int nextLength = length/2;
+			sb.append('(');
+			cntPaper(startRow, startCol, nextLength);
+			cntPaper(startRow, startCol+nextLength, nextLength);
+			cntPaper(startRow+nextLength, startCol, nextLength);
+			cntPaper(startRow+nextLength, startCol+nextLength, nextLength);
+			sb.append(')');
+		}
+		
+	} // end of cntSame()
+	
+	static boolean isSameVal(int startRow, int startCol, int length) {
+		int firstVal=movie[startRow][startCol];
+		
+		for(int i=startRow;i<startRow+length;i++) {
+			for(int j=startCol;j<startCol+length;j++) {
+				if(firstVal!=movie[i][j]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	} // end of isSameColor()
+} // end of Main class
