80fdeba522e04a9a83288493ea542b2c5e14354a
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-06 15:59:06 +0900

    [Gold I] Title: 이항 계수 3, Time: 220 ms, Memory: 45040 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -9,32 +9,36 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static long MOD = 1_000_000_007;
+	static long[] fact;
 	
 	public static void main(String[] args) throws IOException {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		long N = Long.valueOf(st.nextToken());
-		long K = Long.valueOf(st.nextToken());
+		int N = Integer.valueOf(st.nextToken());
+		int K = Integer.valueOf(st.nextToken());
 		
 		// NCK = N!/(N-K)!K! % MOD 계산
+		fact = new long[N+1];
 		bw.write(Long.toString(fact(N)*pow(fact(N-K)*fact(K)%MOD, MOD-2)%MOD));
 		
 		bw.close();
 	} // end of main()
 	
-	static long fact(long N) {
-		long fact=1L;
-		
-		while(N>1) {
-			fact=fact*N%MOD;
-			N--;
+	static long fact(int N) {
+		if(fact[N]!=0) {
+			return fact[N];
+		} else {
+			fact[0]=fact[1]=1;
+			
+			for(int i=2;i<=N;i++) {
+				fact[i]=(fact[i-1]*i)%MOD;
+			}
+			return fact[N];
 		}
 		
-		return fact;
 	} // end of fact()
 	
 	static long pow(long N, long K) {
-		
 		if(K==1) {
 			return N;
 		}
