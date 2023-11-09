063a1dc9e4bfbf5ea1974a87dccfe448ef7616fa
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-09 18:13:34 +0900

    [Gold IV] Title: 키 순서, Time: 616 ms, Memory: 39964 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int INF=25000;
+	
+	public static void main(String[] args) throws IOException {
+
+		// 학생의 수 N, 키를 비교한 횟수 M
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int M = Integer.valueOf(st.nextToken());
+		
+		// 각 학생의 노드 생성
+		int[][] height = new int[N][N];
+		for(int i=0;i<N;i++) {
+			Arrays.fill(height[i], INF);
+			height[i][i]=0;
+		}
+		
+		// a에서 b로 향하는 간선 입력
+		while(M-->0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.valueOf(st.nextToken())-1;
+			int b = Integer.valueOf(st.nextToken())-1;
+			height[a][b]=1;
+		}
+		
+		// 한 노드에서 다른 노드로 가는 최단거리 계산
+		for(int mid=0;mid<N;mid++) {
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					height[i][j] = Math.min(height[i][j], height[i][mid]+height[mid][j]);
+				}
+			}
+		}
+		
+		// 자신의 키가 몇번째인지 알 수 있는 학생의 수 계산
+		// = 자신을 제외한 모든 노드에서 오거나 갈 수 있는지 확인
+		int cnt=0;
+		for(int i=0;i<N;i++) {
+			boolean canCompare=true;
+			for(int j=0;j<N;j++) {
+				if(height[i][j]==INF&&height[j][i]==INF) {
+					canCompare=false;
+					break;
+				}
+			}
+			
+			if(canCompare) {
+				cnt++;
+			}
+		}
+		
+		bw.write(Integer.toString(cnt));
+		
+		bw.close();
+	} // end of main()
+} // end of Main class
