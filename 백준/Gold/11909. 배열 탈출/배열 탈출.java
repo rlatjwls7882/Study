518c8e7a98ec569c8c38cd14ce8af5f68f6243df
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-11 17:15:11 +0900

    [Gold V] Title: 배열 탈출, Time: 1016 ms, Memory: 338404 KB -BaekjoonHub

--- 백준/Gold/11909. 배열 탈출/배열 탈출.java
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.java
@@ -4,34 +4,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int[] moveX = {1, 0};
-	static int[] moveY = {0, 1};
-	static int INF = 1_000_000_000, N;
-	static int[][] routeVal, map;
-	static boolean[][] visited;
 	
 	public static void main(String[] args) throws IOException {
-
-		// 배열의 크기 N
-		N = Integer.valueOf(br.readLine());
+		
+		int N = Integer.valueOf(br.readLine());
 		
 		// 초기화
-		routeVal = new int[N][N];
+		int[][] map = new int[N][N];
+		int[][] cost = new int[N][N];
 		for(int i=0;i<N;i++) {
-			Arrays.fill(routeVal[i], INF);
+			Arrays.fill(cost[i], 1_000_000_000);
 		}
-		routeVal[0][0]=0;
-		visited = new boolean[N][N];
 		
-		// 배열 입력
-		map = new int[N][N];
-		
+		// 맵 입력
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
@@ -39,70 +29,31 @@ public class Main {
 			}
 		}
 		
-		// 상수가 배열을 탈출하기 위해 들여야 할 최소비용 계산
-		dijkstra();
-		bw.write(Integer.toString(routeVal[N-1][N-1]));
-		
-		bw.close();
-	} // end of main()
-	
-	static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0, 0));
-		
-		while(!pq.isEmpty()) {
-			Node curNode = pq.poll();
-			int x = curNode.x;
-			int y = curNode.y;
-			
-			// 한번 간 장소 체크
-			if(visited[x][y]) {
-				continue;
-			} else {
-				visited[x][y]=true;
-			}
-
-			// 탈출
-			if(x==N-1&&y==N-1) {
-				return;
-			}
-
-			// 이동
-			for(int i=0;i<2;i++) {
-
-				int nextX = x+moveX[i];
-				int nextY = y+moveY[i];
-
-				if(x==N-1&&i==0||y==N-1&&i==1||visited[nextX][nextY]) {
-					continue;
-				}
-
-				int nextRouteVal=routeVal[x][y];
-				if(map[x][y]<=map[nextX][nextY]) {
-					nextRouteVal+=map[nextX][nextY]-map[x][y]+1;
-				}
-
-				if(routeVal[nextX][nextY]>nextRouteVal) {
-					routeVal[nextX][nextY]=nextRouteVal;
-					pq.add(new Node(nextX, nextY, nextRouteVal));
+		// 배열을 탈출하기 위해 들여야 할 최소 비용 계산
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(i==0&&j==0) {
+					cost[0][0]=0;
+				} else if(i==0||j==0) {
+					int beforeX=i, beforeY=j;
+					if(i==0) {
+						beforeY--;
+					} else {
+						beforeX--;
+					}
+					
+					cost[i][j]=cost[beforeX][beforeY]+Math.max(0, map[i][j]-map[beforeX][beforeY]+1);
+				} else {
+					int cost1 = cost[i-1][j]+Math.max(0, map[i][j]-map[i-1][j]+1);
+					int cost2 = cost[i][j-1]+Math.max(0, map[i][j]-map[i][j-1]+1);
+					
+					cost[i][j] = Math.min(cost1, cost2);
 				}
 			}
 		}
-
-	} // end of dijkstra()
+		
+		bw.write(Integer.toString(cost[N-1][N-1]));
+		
+		bw.close();
+	} // end of main()
 } // end of Main class
-
-class Node implements Comparable<Node> {
-	int x, y, cost;
-	
-	public Node(int x, int y, int cost) {
-		this.x=x;
-		this.y=y;
-		this.cost=cost;
-	}
-	
-	@Override
-	public int compareTo(Node node) {
-		return this.cost-node.cost;
-	}
-} // end of Node class
