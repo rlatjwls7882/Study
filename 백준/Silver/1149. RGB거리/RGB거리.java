59da85e536941bc953b3e7f36520b9338ca93823
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-11 19:03:07 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -14,21 +14,24 @@ public class Main {
 		// 집의 수 N
 		int N = Integer.valueOf(br.readLine());
 		
-		int[] cost = new int[3];
-		while(N-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		// 첫번째 집 칠하기
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[][] cost = new int[N][3];
+		for(int i=0;i<3;i++) {
+			cost[0][i] = Integer.valueOf(st.nextToken());
+		}
+		
+		// 다음 집 칠하기
+		for(int i=1;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
 			
-			// 다음 집을 칠하는 최소 비용 계산
-			int[] next = new int[3];
-			next[0] = Integer.valueOf(st.nextToken())+Math.min(cost[1], cost[2]);
-			next[1] = Integer.valueOf(st.nextToken())+Math.min(cost[0], cost[2]);
-			next[2] = Integer.valueOf(st.nextToken())+Math.min(cost[0], cost[1]);
-			
-			cost = next.clone();
+			cost[i][0] = Integer.valueOf(st.nextToken())+Math.min(cost[i-1][1], cost[i-1][2]);
+			cost[i][1] = Integer.valueOf(st.nextToken())+Math.min(cost[i-1][0], cost[i-1][2]);
+			cost[i][2] = Integer.valueOf(st.nextToken())+Math.min(cost[i-1][0], cost[i-1][1]);
 		}
 		
 		// 모든 집을 칠하는 최소 비용 출력
-		bw.write(Integer.toString(Math.min(Math.min(cost[0], cost[1]), cost[2])));
+		bw.write(Integer.toString(Math.min(Math.min(cost[N-1][0], cost[N-1][1]), cost[N-1][2])));
 		
 		bw.close();
 	} // end of main()
