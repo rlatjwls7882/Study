327e462f76903e2e0475bb3c7ffbf4858b761d30
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-17 16:59:57 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 148 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -24,12 +24,11 @@ public class Main {
 		
 		// 가장 큰 증가하는 부분 수열 계산
 		int[] dp = new int[N];
-		for(int i=0;i<N;i++) {
-			dp[i]=A[i];
-		}
-		
-		int max=dp[0];
+		int max=dp[0]=A[0];
+
 		for(int i=1;i<N;i++) {
+			dp[i]=A[i];
+			
 			for(int j=0;j<i;j++) {
 				if(A[j]<A[i]) {
 					dp[i] = Math.max(dp[j]+A[i], dp[i]);
