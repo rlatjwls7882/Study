3328ba0a17ebcc85cff41486333729232dbe5461
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-11-24 22:47:48 +0900

    Update 무지개 만들기.java

--- 백준/Bronze/30403. 무지개 만들기/무지개 만들기.java
+++ 백준/Bronze/30403. 무지개 만들기/무지개 만들기.java
@@ -13,47 +13,35 @@ public class Main {
 		int N = Integer.valueOf(br.readLine());
 		String string = br.readLine();
 		
-		// R O Y G B I V가 있는지 확인
-		boolean[][] chars = new boolean[7][2];
+		// 문자열의 구성요소 확인
+		boolean[][] chars = new boolean[26][2];
 		
 		for(int i=0;i<N;i++) {
-			if(string.charAt(i)=='r') {
-				chars[0][0]=true;
-			} else if(string.charAt(i)=='R') {
-				chars[0][1]=true;
-			} else if(string.charAt(i)=='o') {
-				chars[1][0]=true;
-			} else if(string.charAt(i)=='O') {
-				chars[1][1]=true;
-			} else if(string.charAt(i)=='y') {
-				chars[2][0]=true;
-			} else if(string.charAt(i)=='Y') {
-				chars[2][1]=true;
-			} else if(string.charAt(i)=='g') {
-				chars[3][0]=true;
-			} else if(string.charAt(i)=='G') {
-				chars[3][1]=true;
-			} else if(string.charAt(i)=='b') {
-				chars[4][0]=true;
-			} else if(string.charAt(i)=='B') {
-				chars[4][1]=true;
-			} else if(string.charAt(i)=='i') {
-				chars[5][0]=true;
-			} else if(string.charAt(i)=='I') {
-				chars[5][1]=true;
-			} else if(string.charAt(i)=='v') {
-				chars[6][0]=true;
-			} else if(string.charAt(i)=='V') {
-				chars[6][1]=true;
+			if('a'<=string.charAt(i)&&string.charAt(i)<='z') {
+				chars[string.charAt(i)-'a'][0]=true;
+			} else {
+				chars[string.charAt(i)-'A'][1]=true;
 			}
 		}
 		
 		// 무지개를 만들 수 있는지 확인
-		if(chars[0][0]&&chars[1][0]&&chars[2][0]&&chars[3][0]&&chars[4][0]&&chars[5][0]&&chars[6][0]&&chars[0][1]&&chars[1][1]&&chars[2][1]&&chars[3][1]&&chars[4][1]&&chars[5][1]&&chars[6][1]) {
+		int[] find = {17, 14, 24, 6, 1, 8, 21};
+		int low=0, upper=0;
+		
+		for(int i=0;i<7;i++) {
+			if(chars[find[i]][0]) {
+				low++;
+			}
+			if(chars[find[i]][1]) {
+				upper++;
+			}
+		}
+		
+		if(low==7&&upper==7) {
 			bw.write("YeS");
-		} else if(chars[0][0]&&chars[1][0]&&chars[2][0]&&chars[3][0]&&chars[4][0]&&chars[5][0]&&chars[6][0]) {
+		} else if(low==7) {
 			bw.write("yes");
-		} else if(chars[0][1]&&chars[1][1]&&chars[2][1]&&chars[3][1]&&chars[4][1]&&chars[5][1]&&chars[6][1]) {
+		} else if(upper==7) {
 			bw.write("YES");
 		} else {
 			bw.write("NO!");
