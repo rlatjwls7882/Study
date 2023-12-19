890fc603470912a2ebfa4213d75930a7da187df9
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-12-20 00:32:25 +0900

    [Bronze I] Title: 옷걸이, Time: 532 ms, Memory: 74512 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.java
+++ 백준/Bronze/27951. 옷걸이/옷걸이.java
@@ -15,34 +15,40 @@ public class Main {
 		int N = Integer.valueOf(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int hangerU=0, hangerD=0;
+		int U=0, D=0;
 		int[] A = new int[N];
 		
 		for(int i=0;i<N;i++) {
 			A[i] = Integer.valueOf(st.nextToken());
 			if(A[i]==1) {
-				hangerU++;
+				U++;
 			} else if(A[i]==2) {
-				hangerD++;
+				D++;
 			}
 		}
 		
 		// 상의와 하의의 개수
 		st = new StringTokenizer(br.readLine());
-		int U = Integer.valueOf(st.nextToken());
-		int D = Integer.valueOf(st.nextToken());
+		U -= Integer.valueOf(st.nextToken());
+		D -= Integer.valueOf(st.nextToken());
 		
 		// 옷걸이에 옷을 어떻게 걸었는지 출력
-		if(hangerU-U>0||hangerD-D>0) {
+		if(U>0||D>0) {
 			bw.write("NO");
 		} else {
 			bw.write("YES\n");
 			for(int i=0;i<N;i++) {
-				if(A[i]==1||A[i]==3&&U>0) {
-					U--;
+				if(A[i]==1) {
 					bw.write('U');
-				} else {
+				} else if(A[i]==2) {
 					bw.write('D');
+				} else {
+					if(U<0) {
+						U++;
+						bw.write('U');
+					} else {
+						bw.write('D');
+					}
 				}
 			}
 		}
