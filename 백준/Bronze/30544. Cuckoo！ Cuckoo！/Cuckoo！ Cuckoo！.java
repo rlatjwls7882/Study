6db8f030c8d3ac9eba66f1537484c573f69b8d5f
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-01-04 00:04:04 +0900

    [Bronze III] Title: Cuckoo! Cuckoo!, Time: 128 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Bronze/30544. Cuckoo！ Cuckoo！/Cuckoo！ Cuckoo！.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int HH, MM;
+	
+	public static void main(String[] args) throws IOException {
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+		HH = Integer.valueOf(st.nextToken());
+		MM = Integer.valueOf(st.nextToken());
+		int N = Integer.valueOf(br.readLine());
+		
+		// N번째 소리가 울려퍼지는 시간 계산
+		while(true) {
+			if(MM==0) {
+				N -= HH;
+			} else if(MM%15==0) {
+				N -= 1;
+			}
+			
+			if(N<=0) {
+				bw.write(String.format("%02d:%02d", HH, MM));
+				break;
+			}
+			
+			MM++;
+			if(MM==60) {
+				MM=0;
+				HH++;
+				if(HH==13) {
+					HH=1;
+				}
+			}
+		}
+		
+		bw.close();
+	} // end of main()
+} // end of Main class
