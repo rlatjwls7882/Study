c4dff17f4b28708884d17ceda5a916ad1a8a930c
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-01-04 19:59:52 +0900

    Update 나는 학급회장이다.java

--- 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.java
+++ 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,96 +15,54 @@ public class Main {
 		int N = Integer.valueOf(br.readLine());
 		
 		// 투표 결과 확인
-		int[] totalScore = new int[3];
-		int[] score3 = new int[3];
-		int[] score2 = new int[3];
+		Score[] scores = new Score[3];
+		for(int i=0;i<3;i++) {
+			scores[i] = new Score(i+1);
+		}
 		
 		while(N-->0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			for(int i=0;i<3;i++) {
 				int num = Integer.valueOf(st.nextToken());
-				totalScore[i] += num;
+				scores[i].totalScore += num;
 				if(num==3) {
-					score3[i]++;
+					scores[i].cnt3 += num;
 				} else if(num==2) {
-					score2[i]++;
+					scores[i].cnt2 += num;
 				}
 			}
 		}
+		Arrays.sort(scores);
 		
 		// 누가 회장으로 당선되었는지 확인
-		int winner = chkWinner(totalScore, score3, score2);
-		
-		if(winner==0) {
-			bw.write(String.format("0 %d", Math.max(Math.max(totalScore[0], totalScore[1]), totalScore[2])));
+		if(scores[0].totalScore>scores[1].totalScore||scores[0].cnt3>scores[1].cnt3||scores[0].cnt2>scores[1].cnt2) {
+			bw.write(String.format("%d %d", scores[0].idx, scores[0].totalScore));
 		} else {
-			bw.write(String.format("%d %d", winner, totalScore[winner-1]));
+			bw.write(String.format("0 %d", scores[0].totalScore));
 		}
 		
 		bw.close();
 	} // end of main()
+} // end of Main class
+
+class Score implements Comparable<Score> {
+	int idx, totalScore, cnt3, cnt2;
 	
-	static int chkWinner(int[] totalScore, int[] score3, int[] score2) {
-		if(totalScore[0]>totalScore[1]&&totalScore[0]>totalScore[2]) {
-			return 1;
-		} else if(totalScore[1]>totalScore[0]&&totalScore[1]>totalScore[2]) {
-			return 2;
-		} else if(totalScore[2]>totalScore[0]&&totalScore[2]>totalScore[1]) {
-			return 3;
-		} else if(totalScore[0]==totalScore[1]&&totalScore[0]!=totalScore[2]) {
-			if(score3[0]>score3[1]) {
-				return 1;
-			} else if(score3[1]>score3[0]) {
-				return 2;
-			} else {
-				if(score2[0]>score2[1]) {
-					return 1;
-				} else if(score2[1]>score2[0]) {
-					return 2;
-				}
-			}
-		} else if(totalScore[0]==totalScore[2]&&totalScore[0]!=totalScore[1]) {
-			if(score3[0]>score3[2]) {
-				return 1;
-			} else if(score3[2]>score3[0]) {
-				return 3;
-			} else {
-				if(score2[0]>score2[2]) {
-					return 1;
-				} else if(score2[2]>score2[0]) {
-					return 3;
-				}
-			}
-		} else if(totalScore[1]==totalScore[2]&&totalScore[0]!=totalScore[1]) {
-			if(score3[1]>score3[2]) {
-				return 2;
-			} else if(score3[2]>score3[1]) {
-				return 3;
-			} else {
-				if(score2[1]>score2[2]) {
-					return 2;
-				} else if(score2[2]>score2[1]) {
-					return 3;
-				}
-			}
+	public Score(int idx) {
+		this.idx=idx;
+	}
+	
+	@Override
+	public int compareTo(Score o) {
+		if(this.totalScore!=o.totalScore) {
+			return o.totalScore-this.totalScore;
 		} else {
-			if(score3[0]>score3[1]&&score3[0]>score3[2]) {
-				return 1;
-			} else if(score3[1]>score3[0]&&score3[1]>score3[2]) {
-				return 2;
-			} else if(score3[2]>score3[0]&&score3[2]>score3[1]) {
-				return 3;
+			if(this.cnt3!=o.cnt3) {
+				return o.cnt3-this.cnt3;
 			} else {
-				if(score2[0]>score2[1]&&score2[0]>score2[2]) {
-					return 1;
-				} else if(score2[1]>score2[0]&&score2[1]>score2[2]) {
-					return 2;
-				} else if(score2[2]>score2[0]&&score2[2]>score2[1]) {
-					return 3;
-				}
+				return o.cnt2-this.cnt2;
 			}
 		}
-		return 0;
-	} // end of chkWinner()
-} // end of Main class
+	}
+} // end of Score class
