f777d36be51f4e533a5d87138702eed4428b4905
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-01-30 21:30:20 +0900

    [Bronze II] Title: GBus count (Small), Time: 132 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/12184. GBus count （Small）/GBus count （Small）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		
+		int T = Integer.valueOf(br.readLine());
+		
+		for(int i=1;i<=T;i++) {
+			if(i!=1) {
+				br.readLine();
+			}
+			int N = Integer.valueOf(br.readLine());
+			
+			int[] cnt = new int[501];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			while(N-->0) {
+				int A = Integer.valueOf(st.nextToken());
+				int B = Integer.valueOf(st.nextToken());
+				while(A<=B) {
+					cnt[A++]++;
+				}
+			}
+			
+			int P = Integer.valueOf(br.readLine());
+			bw.write("Case #"+i+": ");
+			
+			while(P-->0) {
+				bw.write(cnt[Integer.valueOf(br.readLine())]+" ");
+			}
+			bw.newLine();
+		}
+
+		bw.close();
+	} // end of main()
+} // end of Main class
