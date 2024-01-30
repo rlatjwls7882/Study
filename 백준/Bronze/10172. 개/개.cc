69d4d4badfc24a4600802552d23dc6b2ed452a7f
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-01-31 02:51:48 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "|\\_/|" << std::endl
+              << "|q p|   /}" << std::endl
+              << "( 0 )\"\"\"\\" << std::endl
+              << "|\"^\"`    |" << std::endl
+              << "||_/=\\\\__|";
+}
