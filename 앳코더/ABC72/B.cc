4381e6c95df9091889bb7a11596d68e11eac1cbf
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-02-01 03:39:17 +0900

    2/1

---
+++ 앳코더/ABC72/B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+
+  for (int i = 0; i < str.length(); i++) {
+    if (i % 2 == 0) {
+      cout << str[i];
+    }
+  }
+}
