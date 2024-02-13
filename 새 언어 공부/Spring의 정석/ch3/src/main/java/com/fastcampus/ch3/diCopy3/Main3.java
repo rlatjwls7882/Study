f4160191825e64e73e947dc1fc5ad4e1b5811469
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-02-14 08:20:21 +0900

    파일정리

--- 새 언어 공부/Spring의 정석/ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
+++ 새 언어 공부/Spring의 정석/ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -48,8 +48,17 @@ class AppContext {
         }
     }
 
-    Object getBean(String key) {
+    Object getBean(String key) { // byName
         return map.get(key);
+    }
+
+    Object getBean(Class clazz) { // byType
+        for(Object obj:map.values()) {
+            if(clazz.isInstance(obj)) {
+                return obj;
+            }
+        }
+        return null;
     }
 }
 
@@ -57,9 +66,11 @@ public class Main3 {
     public static void main(String[] args) throws Exception {
         AppContext ac = new AppContext();
 
-        Car car = (Car)ac.getBean("car");
+        Car car = (Car)ac.getBean("car"); // byName으로 객체를 검색
+        Car car2 = (Car)ac.getBean(Car.class); // byType으로 객체를 검색
         Engine engine = (Engine)ac.getBean("engine");
         System.out.println("car = " + car);
+        System.out.println("car2 = " + car2);
         System.out.println("engine = " + engine);
     }
 }
