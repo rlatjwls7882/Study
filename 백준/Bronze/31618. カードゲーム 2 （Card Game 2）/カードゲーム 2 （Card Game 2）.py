eda6998460d5bd6693f2ff62e43cb8155c2d43fe
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-03-15 08:51:44 +0900

    [Bronze II] Title: カードゲーム 2 (Card Game 2), Time: 116 ms, Memory: 53548 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.py
+++ 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.py
@@ -6,7 +6,7 @@ for i in range(N):
     card[A[i]]=True
 
 check=False
-for i in range(1, 199_994):
+for i in range(1, 199_995):
     if card[i] and card[i+3] and card[i+6]:
         check=True
         break
