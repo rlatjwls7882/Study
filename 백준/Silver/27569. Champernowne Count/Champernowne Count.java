3cfe75e8af6f1b14b12e359ec2df9914e846e20a
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-03-18 15:39:06 +0900

    [Silver III] Title: Champernowne Count, Time: 140 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/27569. Champernowne Count/Champernowne Count.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long N = Integer.valueOf(st.nextToken());
+		long K = Integer.valueOf(st.nextToken());
+        
+		int cnt=0;
+		long remainder=0;
+		for(long i=1;i<=N;i++) {
+			remainder = (remainder*(long)Math.pow(10, (long)(Math.log10(i)+1))+i)%K;
+			if(remainder==0) {
+				cnt++;
+			}
+		}
+		bw.write(cnt+"");
+		
+		bw.close();
+	} // end of main()
+} // end of Main class
