bf63e7c14766afc9c506180ea22deb9ca2cf87dd
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-03-22 07:47:53 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int A, B, C; cin >> A >> B >> C;
+    
+    if(A+B==C) {
+        cout << "correct!";
+    } else {
+        cout << "wrong!";
+    }
+}
