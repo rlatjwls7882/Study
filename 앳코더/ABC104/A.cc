2c8a0f384e56a80456df35409009cdf82784bc74
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-04-01 07:27:50 +0900

    4/1

---
+++ 앳코더/ABC104/A.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int R; cin >> R;
+    cout << (R<1200?"ABC":R<2800?"ARC":"AGC");
+}
