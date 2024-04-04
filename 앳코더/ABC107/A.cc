5b9666f16e72e92ae8c16519776f3d819baaca05
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-04-05 07:46:15 +0900

    4/5

---
+++ 앳코더/ABC107/A.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int N, i; cin >> N >> i;
+
+    cout << N-i+1;
+}
---
+++ 코드포스/959/A.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int N; cin >> N;
+
+    cout << (N%2==0?"Mahmoud":"Ehab");
+}
