4f329c4e4152a4ca4a19ce9d53db1f13961cb00c
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-04-04 20:27:31 +0900

    4/4

---
+++ 코드포스/1371/A.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int t; cin >> t;
+    
+    while(t-->0) {
+        int n; cin >> n;
+        cout << (n+1)/2 << endl;
+    }
+}
