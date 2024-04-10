070239bcb7a64fc26604aea9d73ad66d4bc93374
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-04-10 09:50:55 +0900

    4/10

---
+++ 앳코더/ABC112/A.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int N; cin >> N;
+
+    if(N==1) {
+        cout << "Hello World";
+    } else {
+        int A, B; cin >> A >> B;
+        cout << A+B;
+    }
+}
