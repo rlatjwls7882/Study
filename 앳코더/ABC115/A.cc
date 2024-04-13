6ca074c347ff3e8d376af552dfde20930be34cbf
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-04-13 10:18:22 +0900

    4/13

---
+++ 앳코더/ABC115/A.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio; cin.tie(); cout.tie();
+    int D; cin >> D;
+
+    cout << "Christmas";
+    for(int i=D;i<=24;i++) {
+        cout << " Eve";
+    }
+}
