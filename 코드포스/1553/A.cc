2c41d381835b267d6d8a104f87910631b715c1cd
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-05-02 13:59:17 +0900

    5/2

---
+++ 코드포스/1553/A.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t-->0) {
+        int n; cin >> n;
+        cout << (n+1)/10 << endl;
+    }
+}
