4377e8ffceda4c90934e1cb26808eb9e0125d665
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-05-08 13:39:59 +0900

    5/8

---
+++ 앳코더/ABC197/A.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string s; cin >> s;
+    cout << s[1] << s[2] << s[0];
+}
