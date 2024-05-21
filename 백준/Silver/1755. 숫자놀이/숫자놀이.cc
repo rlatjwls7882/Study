07ff0871f652ab3b39c3437e8c221e89f632dfe6
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-05-21 18:29:41 +0900

    [Silver IV] Title: 숫자놀이, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, string> P;
+
+bool comp(P a, P b) {
+    // if(a.second.length()==b.second.length())
+    return a.second<b.second;
+    // return a.second.length()<b.second.length();
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string num[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int M, N; cin >> M >> N;
+
+    vector<P> v;
+    for(int i=M;i<=N;i++) {
+        string tmp = "";
+        if(i>=10) tmp = num[i/10] + " ";
+        tmp += num[i%10];
+        v.push_back(P(i, tmp));
+    }
+    sort(v.begin(), v.end(), comp);
+
+    for(int i=0;i<v.size();i++) {
+        cout << v[i].first << " ";
+        if(i%10==9) cout << '\n';
+    }
+}
