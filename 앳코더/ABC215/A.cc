091b832429b3631c6aef0f07835faeabb20a70a6
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-05-29 10:56:37 +0900

    5/29

---
+++ 앳코더/ABC215/A.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string s; cin >> s;
+    if(s.compare("Hello,World!")==0) {
+        cout << "AC";
+    } else {
+        cout << "WA";
+    }
+}
