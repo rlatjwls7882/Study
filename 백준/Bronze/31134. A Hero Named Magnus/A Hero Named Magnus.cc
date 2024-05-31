66f185094025a04702eadbbe43609e20f3c73022
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-05-31 19:36:27 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t-->0) {
+        long long x; cin >> x;
+        cout << x*2-1 << '\n';
+    }
+}
