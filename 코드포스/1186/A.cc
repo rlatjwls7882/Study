b1f52e7fa9b53419b340601777ef10b2d540fe2d
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-06-10 12:32:14 +0900

    6/10

---
+++ 코드포스/1186/A.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m, k; cin >> n >> m >> k;
+    if(n<=m && n<=k) cout << "Yes";
+    else cout << "No";
+}
