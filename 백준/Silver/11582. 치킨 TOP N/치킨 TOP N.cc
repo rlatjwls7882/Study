4043c3974ebccbeaee3a01d40ab5e4d89529a5d1
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-06-21 21:29:28 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 240 ms, Memory: 6180 KB -BaekjoonHub

---
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    int arr[N];
+    for(int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+
+    int k; cin >> k;
+    int cnt=0;
+    while(k<N) {
+        k*=2;
+        cnt++;
+    }
+    k = pow(2, cnt);
+    
+    for(int i=0;i<N;i+=k) {
+        sort(arr+i, arr+i+k);
+    }
+
+    for(int i=0;i<N;i++) {
+        cout << arr[i] << ' ';
+    }
+}
