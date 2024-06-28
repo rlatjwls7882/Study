47d33a0cff64a9db90f810f7157dee82a6afcbb9
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-06-29 00:16:30 +0900

    6/29

---
+++ 앳코더/ABC217/A.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string S, T; cin >> S >> T;
+    if(S.compare(T)<0) cout << "Yes";
+    else cout << "No";
+}
