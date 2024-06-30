da855a9ba665f6652b40a2b4021bf5c72082bba3
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-06-30 23:21:26 +0900

    [Silver II] Title: 서로소 싫어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/30620. 서로소 싫어/서로소 싫어.cc
+++ 백준/Silver/30620. 서로소 싫어/서로소 싫어.cc
@@ -4,11 +4,5 @@ using namespace std;
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     long long x, y; cin >> x >> y;
-
-    cout << "2\n";
-    if(x==y) {
-        cout << x << "\n" << -x;
-    } else {
-        cout << x*(y-1) << '\n' << -(x-1)*y;
-    }
+    cout << "2\n" << x*(y-1) << '\n' << -(x-1)*y;
 }
