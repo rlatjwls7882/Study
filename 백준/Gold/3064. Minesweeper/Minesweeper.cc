a1d7cccc1c5e580549c8082614913caf140727f5
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-07-10 03:01:30 +0900

    [Gold IV] Title: Minesweeper, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/3064. Minesweeper/Minesweeper.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T; cin >> T;
+
+    while(T-->0) {
+        int N; cin >> N;
+        string m[N];
+        for(int i=0;i<N;i++) {
+            cin >> m[i];
+        }
+
+        if(N<3) {
+            cout << "0\n";
+            continue;
+        }
+
+        int cnt=0;
+        for(int i=0;i<N-2;i++) {
+            if(m[0][i]>='1') {
+                cnt++;
+                m[0][i]--; m[0][i+1]--; m[0][i+2]--;
+                m[1][i]--;              m[1][i+2]--;
+                m[2][i]--; m[2][i+1]--; m[2][i+2]--;
+            }
+        }
+        for(int i=0;i<N-2;i++) {
+            if(m[i][N-1]>='1') {
+                cnt++;
+                m[i][N-3]--;   m[i][N-2]--;   m[i][N-1]--;
+                m[i+1][N-3]--;                m[i+1][N-1]--;
+                m[i+2][N-3]--; m[i+2][N-2]--; m[i+2][N-1]--;
+            }
+        }
+        for(int i=0;i<N-2;i++) {
+            if(m[N-1][N-1-i]>='1') {
+                cnt++;
+                m[N-3][N-3-i]--; m[N-3][N-2-i]--; m[N-3][N-1-i]--;
+                m[N-2][N-3-i]--;                  m[N-2][N-1-i]--;
+                m[N-1][N-3-i]--; m[N-1][N-2-i]--; m[N-1][N-1-i]--;
+            }
+        }
+        for(int i=0;i<N-2;i++) {
+            if(m[N-1-i][0]>='1') {
+                cnt++;
+                m[N-3-i][0]--; m[N-3-i][1]--; m[N-3-i][2]--;
+                m[N-2-i][0]--;                m[N-2-i][2]--;
+                m[N-1-i][0]--; m[N-1-i][1]--; m[N-1-i][2]--;
+            }
+        }
+        cout << cnt+(max(N-4, 0)*max(N-4, 0)) << '\n';
+    }
+}
