3cb6a6b998387ca28257ba8ef639ff4df03da2f9
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-07-11 15:29:18 +0900

    [Gold V] Title: 트리와 쿼리, Time: 64 ms, Memory: 11020 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> connect = vector<vector<int>>(100001);
+bool visited[100001] = {false, };
+int cnt[100001] = {0, };
+
+int dfs(int cur) {
+    visited[cur]=true;
+    cnt[cur]++;
+    for(int next:connect[cur]) {
+        if(!visited[next]) {
+            cnt[cur] += dfs(next);
+        }
+    }
+    return cnt[cur];
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N, R, Q; cin >> N >> R >> Q;
+    for(int i=0;i<N-1;i++) {
+        int u, v; cin >> u >> v;
+        connect[u].push_back(v);
+        connect[v].push_back(u);
+    }
+
+    dfs(R);
+    
+    while(Q-->0) {
+        int u; cin >> u;
+        cout << cnt[u] << '\n';
+    }
+}
