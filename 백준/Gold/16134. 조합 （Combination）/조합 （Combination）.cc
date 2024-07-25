9a1c183fae105cbe96d7cde4a26a8476416f269a
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-07-26 07:59:15 +0900

    [Gold I] Title: 조합 (Combination), Time: 24 ms, Memory: 25336 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.cc
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.cc
@@ -1,22 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-const long long MOD = 1000000007;
+const ll MOD = 1000000007;
+
+ll factDp[1000001] = {0, };
+ll fact(long long N) {
+    if(N<=1) return factDp[N]=1;
+    if(factDp[N]==0) {
+        factDp[N] = (fact(N-1)*N)%MOD;
+    }
+    return factDp[N];
+}
+
+ll squareRev(long long N, long long K) {
+    if(K==0) return 1;
+    if(K==1) return N;
+
+    long long tmp = squareRev(N, K/2)%MOD;
+    if(K%2==1) return ((tmp*tmp)%MOD*N)%MOD;
+    else return (tmp*tmp)%MOD;
+}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     long long N, R; cin >> N >> R;
-    R = min(R, N-R);
-    long long div=R;
-
-    long long mul=1;
-    for(int i=0;i<R;i++) {
-        mul*=(N-i);
-        while(div>1 && mul%div==0) {
-            mul/=div;
-            div--;
-        }
-        mul%=MOD;
-    }
-    cout << mul;
+    cout << (fact(N)*squareRev(fact(R)*fact(N-R)%MOD, MOD-2))%MOD;
 }
