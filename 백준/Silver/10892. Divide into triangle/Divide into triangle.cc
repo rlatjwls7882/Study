9ddc01d27b946ed584e85d01d9ff3b74f210b9a2
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-08-14 22:16:29 +0900

    [Silver III] Title: Divide into triangle, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10892. Divide into triangle/Divide into triangle.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Stake {
+    int x, y, num;
+    bool operator<(const Stake a) const {
+        if(this->x==a.x) return this->y < a.y;
+        return this->x < a.x;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    vector<Stake> v;
+    for(int i=1;i<=3*N;i++) {
+        int x, y; cin >> x >> y;
+        v.push_back({x, y, i});
+    }
+    sort(v.begin(), v.end());
+
+    for(int i=0;i<3*N;i+=3) {
+        cout << v[i].num << ' ' << v[i+1].num << ' ' << v[i+2].num << '\n';
+    }
+}
