99ade837550ba44ed2b060442ad81b89cd3e6e45
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-08-21 12:07:34 +0900

    [Bronze I] Title: Watering - 1, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/9285. Watering － 1/Watering － 1.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cout << "aa#aa|aaagh\nabcca|bbbgh\nbbcdd|cd#gh\nfeeed|cdeff\nffggg|cdeef\n-----+-----\naaabb|aaabb\ndcccb|c#efb\ndeeef|ceeff\ndggff|cdggh\n#ghhh|ddghh\n-----+-----\naaahh|aaaff\nbbbgh|bbbfg\ncccgg|ccc#g\nddd#f|dddhg\neeeff|eeehh";
+}
+/*
+aa#aa|aaagh
+abcca|bbbgh
+bbcdd|cd#gh
+feeed|cdeff
+ffggg|cdeef
+-----+-----
+aaabb|aaabb
+dcccb|c#efb
+deeef|ceeff
+dggff|cdggh
+#ghhh|ddghh
+-----+-----
+aaahh|aaaff
+bbbgh|bbbfg
+cccgg|ccc#g
+ddd#f|dddhg
+eeeff|eeehh
+*/
