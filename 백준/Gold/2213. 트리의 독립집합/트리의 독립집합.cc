f3d047d222f035c2bedd564251665855f43b9056
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-08-27 22:50:10 +0900

    [Gold I] Title: 트리의 독립집합, Time: 4 ms, Memory: 3048 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
@@ -4,34 +4,33 @@ using namespace std;
 int w[10000];
 bool visited[10000] = {false, };
 int maxW[10000][2] = {0, }; // includeCur, excludeCur
-vector<set<int>> visitNode1(10000);
-vector<set<int>> visitNode2(10000);
 vector<vector<int>> conn(10000);
+set<int> res;
 
 void dfs(int cur) {
     visited[cur]=true;
     maxW[cur][0] = w[cur];
-    visitNode1[cur].insert(cur+1);
     for(int next:conn[cur]) {
         if(!visited[next]) {
             dfs(next);
 
             if(conn[next].size()==1) {
                 maxW[cur][1] += maxW[next][0];
-                visitNode2[cur].merge(visitNode1[next]);
             } else {
                 maxW[cur][0] += maxW[next][1];
-                set<int> tmp = visitNode2[next];
-                visitNode1[cur].merge(tmp);
-
-                if(maxW[next][0]>maxW[next][1]) {
-                    maxW[cur][1] += maxW[next][0];
-                    visitNode2[cur].merge(visitNode1[next]);
-                } else {
-                    maxW[cur][1] += maxW[next][1];
-                    visitNode2[cur].merge(visitNode2[next]);
-                }
+                maxW[cur][1] += max(maxW[next][0], maxW[next][1]);
             }
+        }
+    }
+}
+
+void findNode(int cur, int inOrEx) {
+    visited[cur]=true;
+    if(inOrEx==0) res.insert(cur+1);
+    for(int next:conn[cur]) {
+        if(!visited[next]) {
+            if(inOrEx==0 || maxW[next][0]<=maxW[next][1]) findNode(next, 1);
+            else findNode(next, 0);
         }
     }
 }
@@ -50,11 +49,10 @@ int main(){
     }
     dfs(0);
 
-    if(maxW[0][0]>maxW[0][1]) {
-        cout << maxW[0][0] << '\n';
-        for(int val:visitNode1[0]) cout << val << ' ';
-    } else {
-        cout << maxW[0][1] << '\n';
-        for(int val:visitNode2[0]) cout << val << ' ';
-    }
+    fill(visited, visited+n, false);
+    if(maxW[0][0]>maxW[0][1]) findNode(0, 0);
+    else findNode(0, 1);
+
+    cout << max(maxW[0][0], maxW[0][1]) << '\n';
+    for(int node:res) cout << node << ' ';
 }
