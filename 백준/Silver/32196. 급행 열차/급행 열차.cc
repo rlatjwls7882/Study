1541a0f027a117f299bf871027fac0071a8a172a
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-08-29 20:36:14 +0900

    [Silver III] Title: 급행 열차, Time: 24 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/32196. 급행 열차/급행 열차.cc
+++ 백준/Silver/32196. 급행 열차/급행 열차.cc
@@ -1,30 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long N, M, K, X, Y;
-
-struct Node {
-    long long A, B;
-    bool operator<(const Node a) const {
-        return this->A*X - this->B*Y > a.A*X - a.B*Y;
-    }
-};
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> N >> M >> K >> X >> Y;
-    priority_queue<Node> pq;
-    vector<pair<long long, long long>> AB;
-    for(int i=0;i<N;i++) {
+    long long N, M, K, X, Y; cin >> N >> M >> K >> X >> Y;
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    while(N-->0) {
         long long A, B; cin >> A >> B;
-        pq.push({A, B});
+        pq.push(A*X-B*Y);
     }
 
-    long long A=K, B=K;
+    long long sum=K*(X+Y);
     while(M-->0) {
-        A += pq.top().A;
-        B -= pq.top().B;
-        pq.pop();
+        sum += pq.top(); pq.pop();
     }
-    cout << A*X+B*Y;
+    cout << sum;
 }
