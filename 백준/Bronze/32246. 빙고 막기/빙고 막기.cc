6c062b660f6a8a6d295fcc78237193fc303a88fe
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-11 14:28:56 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    if(N==2) cout << 3;
+    else cout << N;
+}
