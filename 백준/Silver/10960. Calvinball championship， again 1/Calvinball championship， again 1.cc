24b6b650a257ae6e7dee19d51ff5ded42f1a5479
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-14 16:59:16 +0900

    [Silver V] Title: Calvinball championship, again 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10960. Calvinball championship， again 1/Calvinball championship， again 1.cc
+++ 백준/Silver/10960. Calvinball championship， again 1/Calvinball championship， again 1.cc
@@ -1,47 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, m;
+vector<set<int>> teams, minSizeTeam(7);
 bool dislike[7][7];
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n, m; cin >> n >> m;
-    vector<set<int>> teams;
-    vector<pair<int, int>> cnt;
-    for(int i=0;i<n;i++) cnt.push_back({0, i});
-    while(m-->0) {
-        int a, b; cin >> a >> b;
-        dislike[a-1][b-1]=true;
-        dislike[b-1][a-1]=true;
-        cnt[a-1].first++;
-        cnt[b-1].first++;
+void dfs(int depth) {
+    if(depth==n) {
+        if(minSizeTeam.size()>teams.size()) {
+            minSizeTeam = teams;
+        }
+        return;
     }
-    sort(cnt.begin(), cnt.end());
-
-    for(int i=0;i<n;i++) {
-        bool chk=false;
-        for(int j=0;j<teams.size();j++) {
-            bool notDislike=true;
-            for(int people:teams[j]) {
-                if(dislike[cnt[i].second][people]) {
-                    notDislike=false;
-                    break;
-                }
-            }
-            if(notDislike) {
-                chk=true;
-                teams[j].insert(cnt[i].second);
+    for(int i=0;i<teams.size();i++) {
+        bool chk=true;
+        for(int member:teams[i]) {
+            if(dislike[depth][member]) {
+                chk=false;
                 break;
             }
         }
-        if(!chk) teams.push_back({cnt[i].second});
-    }
-
-    cout << teams.size() << '\n';
-    for(int i=0;i<teams.size();i++) {
-        for(int people:teams[i]) {
-            cout << people+1 << ' ';
+        if(chk) {
+            teams[i].insert(depth);
+            dfs(depth+1);
+            teams[i].erase(depth);
         }
+    }
+    teams.push_back({depth});
+    dfs(depth+1);
+    teams.pop_back();
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    while(m-->0) {
+        int a, b; cin >> a >> b;
+        dislike[a-1][b-1]=dislike[b-1][a-1]=true;
+    }
+    dfs(0);
+
+    cout << minSizeTeam.size() << '\n';
+    for(auto team:minSizeTeam) {
+        for(int member:team) cout << member+1 << ' ';
         cout << '\n';
     }
 }
