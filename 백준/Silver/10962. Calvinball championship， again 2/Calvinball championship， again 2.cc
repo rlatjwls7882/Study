9cec570329520e4472f8cf428639cb6754b2ded1
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-14 16:59:57 +0900

    [Silver I] Title: Calvinball championship, again 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10962. Calvinball championship， again 2/Calvinball championship， again 2.cc
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<set<int>> teams, minSizeTeam(14);
+bool dislike[14][14];
+
+void dfs(int depth) {
+    if(depth==n) {
+        if(minSizeTeam.size()>teams.size()) {
+            minSizeTeam = teams;
+        }
+        return;
+    }
+    for(int i=0;i<teams.size();i++) {
+        bool chk=true;
+        for(int member:teams[i]) {
+            if(dislike[depth][member]) {
+                chk=false;
+                break;
+            }
+        }
+        if(chk) {
+            teams[i].insert(depth);
+            dfs(depth+1);
+            teams[i].erase(depth);
+        }
+    }
+    teams.push_back({depth});
+    dfs(depth+1);
+    teams.pop_back();
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    while(m-->0) {
+        int a, b; cin >> a >> b;
+        dislike[a-1][b-1]=dislike[b-1][a-1]=true;
+    }
+    dfs(0);
+
+    cout << minSizeTeam.size() << '\n';
+    for(auto team:minSizeTeam) {
+        for(int member:team) cout << member+1 << ' ';
+        cout << '\n';
+    }
+}
