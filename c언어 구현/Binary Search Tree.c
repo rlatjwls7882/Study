4d65f4c9eafa2b6f25737f5619f4c73a96593d18
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-10-28 12:59:23 +0900

    Update Binary Search Tree.c

--- c언어 구현/Binary Search Tree.c
+++ c언어 구현/Binary Search Tree.c
@@ -3,146 +3,125 @@
 
 typedef struct node {
     struct node *left, *right, *parent;
-    int key;
+    int val;
 } node;
 
 typedef struct tree {
-    node *child;
+    node *root;
 } tree;
 
-void initTree(tree *tree) {
-    tree->child = NULL;
+void init(tree *t) {
+    t->root=NULL;
 }
 
-void initNode(node *parent, node *node, int key) {
-    node->parent=parent;
-    node->key=key;
-    node->left=node->right=NULL;
-}
-
-void print(node *node) {
-    printf(" %d", node->key);
-    if(node->left) print(node->left);
-    if(node->right) print(node->right);
-}
-
-void insertItem(tree *tree, int key) {
-    node *nextNode = (node*)malloc(sizeof(node));
-    initNode(NULL, nextNode, key);
-
-    if(!tree->child) {
-        tree->child = nextNode;
-        return;
-    }
-
-    node *n = tree->child;
-    while(1) {
-        if(key<n->key) {
-            if(n->left) n = n->left;
-            else {
-                n->left = nextNode;
-                nextNode->parent = n;
-                return;
-            }
-        } else {
-            if(n->right) n = n->right;
-            else {
-                n->right = nextNode;
-                nextNode->parent = n;
-                return;
-            }
+void insertItem(tree *t, int val) {
+    printf("\n");
+    node *n = (node*)malloc(sizeof(node));
+    n->left=n->parent=n->right=NULL;
+    n->val=val;
+    if(t->root==NULL) {
+        t->root = n;
+    } else {
+        node *cur = t->root;
+        while(1) {
+            if(val<cur->val && cur->left) cur = cur->left;
+            else if(val>cur->val && cur->right) cur = cur->right;
+            else break;
         }
+        n->parent=cur;
+        if(val<cur->val) cur->left=n;
+        else cur->right=n;
     }
 }
 
-int searchItem(tree *tree, int key) {
-    if(!tree->child) return 0;
-    node *n = tree->child;
-    while(1) {
-        if(key==n->key) return 1;
-        if(key<n->key && n->left) n = n->left;
-        else if(key>n->key && n->right) n = n->right;
-        else return 0;
-    }
+void print(node *n) {
+    printf(" %d", n->val);
+    if(n->left) print(n->left);
+    if(n->right) print(n->right);
 }
 
-int deleteItem(tree *tree, int key) {
-    if(!tree->child) return 0;
-    node *n = tree->child;
+void printAll(tree *t) {
+    if(t->root) print(t->root);
+    printf("\n");
+}
 
-    while(1) {
-        if(key==n->key) break;
-        if(key<n->key && n->left) n = n->left;
-        else if(key>n->key && n->right) n = n->right;
-        else return 0;
-    }
+int search(node *n, int val) {
+    if(val==n->val) return 1;
+    if(val<n->val && n->left) return search(n->left, val);
+    if(val>n->val && n->right) return search(n->right, val);
+    return 0;
+}
 
-    if(!n->left && !n->right) {
-        if(n->parent==NULL) tree->child=NULL;
-        else if(n->parent->left == n) n->parent->left=NULL;
-        else n->parent->right=NULL;
-    } else if(!n->left) {
-        if(n->parent==NULL) {
-            tree->child = n->right;
-            n->right->parent = NULL;
-        } else {
-            if(n->parent->left == n) n->parent->left = n->right;
-            else n->parent->right = n->right;
-            n->right->parent = n->parent;
-        }
-    } else if(n->left && !n->right) {
-        if(n->parent==NULL) {
-            tree->child = n->left;
-            n->parent=NULL;
-        } else {
-            if(n->parent->left == n) n->parent->left = n->left;
+void searchItem(tree *t, int val) {
+    if(t->root && search(t->root, val)) printf("%d\n", val);
+    else printf("X\n");
+}
+
+int del(node *n, int val, tree* t) {
+    if(val==n->val) {
+        if(!n->left && !n->right) {
+            if(n == t->root) t->root=NULL;
+            else if(n->parent->left==n) n->parent->left=NULL;
+            else n->parent->right=NULL;
+        } else if(n->left && !n->right) {
+            if(n == t->root) t->root=n->left;
+            else if(n->parent->left==n) n->parent->left = n->left;
             else n->parent->right = n->left;
             n->left->parent = n->parent;
-        }
-    } else {
-        node *change = n->right;
-        while(change->left) change = change->left;
-        n->key = change->key;
-        if(!change->right) {
-            if(change->parent->left == change) change->parent->left = NULL;
-            else change->parent->right = NULL;
+        } else if(n->right && !n->left) {
+            if(n == t->root) t->root=n->right;
+            else if(n->parent->left==n) n->parent->left=n->right;
+            else n->parent->right=n->right;
+            n->right->parent = n->parent;
         } else {
-            if(change->parent->left == change) change->parent->left = change->right;
-            else change->parent->right = change->right;
-            change->right->parent = change->parent;
+            node *cur = n->right;
+            while(cur->left) cur = cur->left;
+            n->val = cur->val;
+            if(cur->right) {
+                if(cur->parent->left==cur) cur->parent->left=cur->right;
+                else cur->parent->right=cur->right;
+                cur->right->parent = cur->parent;
+            } else {
+                if(cur->parent->left==cur) cur->parent->left=NULL;
+                else cur->parent->right=NULL;
+            }
         }
+        return 1;
     }
-    return 1;
+    if(val<n->val && n->left) return del(n->left, val, t);
+    if(val>n->val && n->right) return del(n->right, val, t);
+    return 0;
+}
+
+void deleteItem(tree *t, int val) {
+    if(t->root && del(t->root, val, t)) printf("%d\n", val);
+    else printf("X\n");
 }
 
 int main(void) {
-    tree *head = (tree*)malloc(sizeof(tree));
-    initTree(head);
-
-    // format
-    // p -> PreOrder printing
-    // q -> quit
-    // i x -> insert x
-    // s x -> search x
-    // d x -> delete x
+    tree *t = (tree*)malloc(sizeof(tree)); init(t);
+    /* format
+    p -> PreOrder printing
+    q -> quit
+    i x -> insert x
+    s x -> search x
+    d x -> delete x
+    */
     while(1) {
-        char ch; scanf("%c", &ch);
-        if(ch=='q') return 0;
-        if(ch=='p') {
-            getchar(); // enter
-            if(head->child) print(head->child);
-        } else {
-            int key; scanf("%d", &key); getchar(); // enter
-            if(ch=='i') {
-                insertItem(head, key);
-            } else if(ch=='d') {
-                if(deleteItem(head, key)) printf("%d", key);
-                else printf("X");
-            } else {
-                if(searchItem(head, key)) printf("%d", key);
-                else printf("X");
-            }
+        char ch; scanf("%c%*c", &ch);
+        if(ch=='i') {
+            int val; scanf("%d%*c", &val);
+            insertItem(t, val);
+        } else if(ch=='s') {
+            int val; scanf("%d%*c", &val);
+            searchItem(t, val);
+        } else if(ch=='d') {
+            int val; scanf("%d%*c", &val);
+            deleteItem(t, val);
+        } else if(ch=='p') {
+            printAll(t);
+        } else if(ch=='q') {
+            break;
         }
-        printf("\n");
     }
 }
