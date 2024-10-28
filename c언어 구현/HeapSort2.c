17de68d912979d395a6c71c8a9d7db52e0c0822e
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-10-28 12:07:19 +0900

    Update HeapSort2.c

--- c언어 구현/HeapSort2.c
+++ c언어 구현/HeapSort2.c
@@ -1,44 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int arr[101], size;
-
-void swap(int *x, int *y) {
-    int tmp = *x;
-    *x = *y;
-    *y = tmp;
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-int max(int a, int b) {
-    return a<b ? b : a;
-}
-
-void downHeap(int i) {
-    while(i*2<=size && max(arr[i*2], arr[i*2+1])>arr[i]) {
-        if(arr[i*2]>arr[i*2+1]) {
-            swap(&arr[i*2], &arr[i]);
+void downHeap(int *arr, int i, int size) {
+    while(1) {
+        if(i*2+1<=size && arr[i*2+1]>=arr[i*2] && arr[i*2+1]>arr[i]) {
+            swap(&arr[i], &arr[i*2+1]);
+            i = i*2+1;
+        } else if(i*2<=size && arr[i*2]>arr[i]) {
+            swap(&arr[i], &arr[i*2]);
             i = i*2;
         } else {
-            swap(&arr[i*2+1], &arr[i]);
-            i = i*2+1;
+            break;
         }
     }
 }
 
-void heapify(int i) {
-    while(i>=1) {
-        downHeap(i--);
-    }
-}
-
-void printHeap() {
-    for(int i=1;i<=size;i++) printf(" %d", arr[i]);
-    printf("\n");
-}
-
-int main() {
-    scanf("%d", &size);
-    for(int i=1;i<=size;i++) scanf("%d", &arr[i]);
-    heapify(size/2);
-    printHeap();
+int main(void) {
+    int n; scanf("%d", &n);
+    int *arr = (int*)malloc(sizeof(int)*(n+1));
+    for(int i=1;i<=n;i++) scanf("%d", &arr[i]);
+    for(int i=n;i>=1;i--) downHeap(arr, i, n);
+    for(int i=1;i<=n;i++) printf(" %d", arr[i]);
 }
