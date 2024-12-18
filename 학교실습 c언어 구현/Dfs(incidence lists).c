b1788a1a6a8f21236addd14f46dd7a279d38be67
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-12-18 22:16:46 +0900

    dfs

--- 학교실습 c언어 구현/Dfs(incidence lists).c
+++ 학교실습 c언어 구현/Dfs(incidence lists).c
@@ -1,85 +1,129 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 
 typedef struct list {
-    struct list* next;
-    struct edge* connectedEdge;
+    struct list *next;
+    struct edge *e;
 } list;
 
 typedef struct node {
-    list *adj;
-    int val, visited;
+    int nodeNum, visited;
+    list *l;
 } node;
 
 typedef struct edge {
-    node *node1, *node2;
+    node *a, *b;
 } edge;
 
-int getNextNodeNum(edge *e, int from) {
-    if(e->node1->val==from) return e->node2->val;
-    return e->node1->val;
+int getAnotherNodeNum(int nodeNum, edge *e) {
+    if(e->a->nodeNum==nodeNum) return e->b->nodeNum;
+    return e->a->nodeNum;
 }
 
-node* getNextNode(node *n, list* cur) {
-    if(cur->connectedEdge->node1==n) return cur->connectedEdge->node2;
-    return cur->connectedEdge->node1;
+node* getAnotherNode(int nodeNum, edge *e) {
+    if(e->a->nodeNum==nodeNum) return e->b;
+    return e->a;
 }
 
-void connect(node *n, edge *e, int to) {
-    list *newNode = (list*)malloc(sizeof(list));
-    newNode->next=NULL;
-    newNode->connectedEdge = e;
+void insert(node *n, edge *e) {
+    list *l = (list*)malloc(sizeof(list));
+    int to = getAnotherNodeNum(n->nodeNum, e);
+    l->next=NULL;
+    l->e=e;
 
-    list *cur = n->adj;
-    if(cur->next==NULL) {
-        cur->next = newNode;
-    } else {
-        while(cur->next!=NULL && getNextNodeNum(cur->next->connectedEdge, n->val)<to) cur = cur->next;
-        if(cur->next) newNode->next = cur->next;
-        cur->next = newNode;
-    }
-}
-
-void dfs(node *n) {
-    if(n->visited) return;
-    n->visited = 1;
-    printf("%d\n", n->val);
-    list *cur = n->adj;
-
-    while(cur->next) {
-        dfs(getNextNode(n, cur->next));
+    list *cur = n->l;
+    while(cur->next!=NULL) {
+        int nextNodeNum = getAnotherNodeNum(n->nodeNum, cur->next->e);
+        if(to<nextNodeNum) break;
         cur = cur->next;
     }
+    l->next = cur->next;
+    cur->next = l;
 }
 
-void freeList(list *cur) {
-    if(cur->next) freeList(cur->next);
-    free(cur);
+void dfs(node *cur) {
+    list *l = cur->l;
+    cur->visited=1;
+    printf("%d\n", cur->nodeNum);
+    while(l->next) {
+        node *next = getAnotherNode(cur->nodeNum, l->next->e);
+        if(!next->visited) dfs(next);
+        l = l->next;
+    }
 }
 
-int main(void) {
-    int N, M, S; scanf("%d %d %d", &N, &M, &S);
+void freeList(list *l) {
+    if(l->next) freeList(l->next);
+    free(l);
+}
+
+int main() {
+    int N, M, S; scanf("%d%d%d", &N, &M, &S);
     node *n = (node*)malloc(sizeof(node)*N);
     edge *e = (edge*)malloc(sizeof(edge)*M);
 
     for(int i=0;i<N;i++) {
-        n[i].adj = (list*)malloc(sizeof(list));
-        n[i].adj->next=NULL;
-        n[i].val = i+1;
-        n[i].visited = 0;
+        n[i].nodeNum=i+1;
+        n[i].visited=0;
+        n[i].l = (list*)malloc(sizeof(list));
+        n[i].l->next=NULL;
     }
 
     for(int i=0;i<M;i++) {
-        int a, b; scanf("%d %d", &a, &b);
-        e[i].node1 = &n[a-1];
-        e[i].node2 = &n[b-1];
-        connect(&n[a-1], &e[i], b);
-        connect(&n[b-1], &e[i], a);
+        int u, v; scanf("%d%d", &u, &v);
+        e[i].a = &n[u-1];
+        e[i].b = &n[v-1];
+        insert(e[i].a, &e[i]);
+        insert(e[i].b, &e[i]);
     }
 
     dfs(&n[S-1]);
-
-    for(int i=0;i<N;i++) freeList(n[i].adj);
+    for(int i=0;i<N;i++) freeList(n[i].l);
     free(n);
     free(e);
 }
+
+/*
+Input1
+5 7 1
+1 2
+1 4
+5 1
+3 5
+4 3
+3 1
+2 3
+
+Output1
+1
+2
+3
+4
+5
+
+Input2
+8 12 7
+1 2
+2 4
+4 7
+3 6
+6 1
+7 6
+7 8
+1 3
+2 7
+1 4
+2 5
+7 5
+
+Output2
+7
+2
+1
+3
+6
+4
+5
+8
+*/
