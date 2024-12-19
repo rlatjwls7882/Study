d5984186a91315b8bfa458176a5375ba27d274f5
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-12-19 14:49:41 +0900

    Update Prim-Jarnik.c

--- 학교실습 c언어 구현/Prim-Jarnik.c
+++ 학교실습 c언어 구현/Prim-Jarnik.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
-#include<memory.h>
-#define INF 1000000000
 
-/********** Graph ***********/
 typedef struct list {
     struct list *next;
     struct edge *e;
 } list;
 
 typedef struct node {
-    list *list;
-    int nodeNum, toCost;
+    int nodeNum, visited;
+    list *l;
 } node;
 
 typedef struct edge {
@@ -21,26 +19,24 @@ typedef struct edge {
 
 void insert(node *n, edge *e) {
     list *l = (list*)malloc(sizeof(list));
-    l->next = NULL;
+    l->next = n->l->next;
     l->e = e;
-    list *cur = n->list;
-    while(cur->next) cur = cur->next;
-    cur->next = l;
+    n->l->next = l;
 }
 
-node* getAnotherNode(node *n, edge *e) {
-    if(e->a == n) return e->b;
-    return e->a;
+void freeList(list* l) {
+    if(l->next) freeList(l->next);
+    free(l);
 }
-/********** Graph ***********/
 
-/********** Prioirity Queue ***********/
+/* PriorityQueue */
 typedef struct pqNode {
-    int nodeNum, cost;
+    node *n;
+    int cost;
 } pqNode;
 
 typedef struct priority_queue {
-    pqNode *heap;
+    pqNode *arr;
     int size;
 } priority_queue;
 
@@ -50,15 +46,20 @@ void swap(pqNode *a, pqNode *b) {
     *b = tmp;
 }
 
-void pqInsert(priority_queue *pq, int nodeNum, int cost) {
-    pq->size++;
-    pq->heap[pq->size].nodeNum=nodeNum;
-    pq->heap[pq->size].cost=cost;
+node* getAnotherNode(node *n, edge *e) {
+    if(n == e->a) return e->b;
+    return e->a;
+}
 
-    int idx=pq->size;
+void push(priority_queue *pq, edge *e, node *n) {
+    pq->size++;
+    pq->arr[pq->size].n = getAnotherNode(n, e);
+    pq->arr[pq->size].cost = e->cost;
+
+    int idx = pq->size;
     while(idx>1) {
-        if(pq->heap[idx].cost < pq->heap[idx/2].cost) {
-            swap(&pq->heap[idx], &pq->heap[idx/2]);
+        if(pq->arr[idx].cost < pq->arr[idx/2].cost) {
+            swap(&pq->arr[idx], &pq->arr[idx/2]);
             idx/=2;
         } else {
             break;
@@ -66,45 +67,67 @@ void pqInsert(priority_queue *pq, int nodeNum, int cost) {
     }
 }
 
-typedef struct pair {
-    int nodeNum, cost;
-} pair;
-
-pair pop(priority_queue *pq) {
-    pair p = {pq->heap[1].nodeNum, pq->heap[1].cost};
-    swap(&pq->heap[1], &pq->heap[pq->size]);
-    pq->size--;
-
+pqNode top(priority_queue *pq) {
+    pqNode a = pq->arr[1];
+    pq->arr[1] = pq->arr[pq->size--];
     int idx=1;
-    while(1) {
-        if(idx*2+1<=pq->size && pq->heap[idx*2+1].cost<pq->heap[idx*2].cost && pq->heap[idx*2+1].cost < pq->heap[idx].cost) {
-            swap(&pq->heap[idx], &pq->heap[idx*2+1]);
+    while(idx<=pq->size) {
+        if(idx*2+1<=pq->size && pq->arr[idx].cost > pq->arr[idx*2+1].cost && pq->arr[idx*2+1].cost < pq->arr[idx*2].cost) {
+            swap(&pq->arr[idx], &pq->arr[idx*2+1]);
             idx = idx*2+1;
-        } else if(idx*2<=pq->size && pq->heap[idx*2].cost < pq->heap[idx].cost) {
-            swap(&pq->heap[idx], &pq->heap[idx*2]);
-            idx*=2;
+        } else if(idx*2<=pq->size && pq->arr[idx].cost > pq->arr[idx*2].cost) {
+            swap(&pq->arr[idx], &pq->arr[idx*2]);
+            idx = idx*2;
         } else {
             break;
         }
     }
-    return p;
+    return a;
 }
-/********** Prioirity Queue ***********/
+/* PriorityQueue */
 
-void freeList(list *l) {
-    if(l->next) freeList(l->next);
-    free(l);
+void Prim_Jarnik(node *n, edge *e, int N, int M) {
+    priority_queue *pq = (priority_queue*)malloc(sizeof(priority_queue));
+    pq->arr = (pqNode*)malloc(sizeof(pqNode)*1001);
+    pq->size=0;
+
+    printf(" 1");
+    n[0].visited=1;
+    list *l = n[0].l;
+    while(l->next) {
+        l = l->next;
+        push(pq, l->e, &n[0]);
+    }
+
+    int totalCost=0;
+    while(pq->size) {
+        pqNode cur = top(pq);
+        if(cur.n->visited) continue;
+        printf(" %d", cur.n->nodeNum);
+        cur.n->visited = 1;
+        totalCost += cur.cost;
+
+        l = cur.n->l;
+        while(l->next) {
+            l = l->next;
+            push(pq, l->e, cur.n);
+        }
+    }
+    printf("\n%d", totalCost);
+    free(pq->arr);
+    free(pq);
 }
 
-int main(void) {
+int main() {
     int N, M; scanf("%d %d", &N, &M);
     node *n = (node*)malloc(sizeof(node)*N);
     edge *e = (edge*)malloc(sizeof(edge)*M);
+
     for(int i=0;i<N;i++) {
-        n[i].list = (list*)malloc(sizeof(list));
-        n[i].list->next = NULL;
-        n[i].nodeNum=i;
-        n[i].toCost=INF;
+        n[i].l = (list*)malloc(sizeof(list));
+        n[i].l->next = NULL;
+        n[i].nodeNum = i+1;
+        n[i].visited = 0;
     }
 
     for(int i=0;i<M;i++) {
@@ -112,40 +135,28 @@ int main(void) {
         e[i].a = &n[a-1];
         e[i].b = &n[b-1];
         e[i].cost = c;
-        insert(&n[a-1], &e[i]);
-        insert(&n[b-1], &e[i]);
+        insert(e[i].a, &e[i]);
+        insert(e[i].b, &e[i]);
     }
+    Prim_Jarnik(n, e, N, M);
 
-    // Priority_Queue Init
-    priority_queue *pq = (priority_queue*)malloc(sizeof(priority_queue));
-    pq->heap = (pqNode*)malloc(sizeof(pqNode)*100000);
-    pq->size=0;
-
-    // Prim-Jarnik
-    int sum=0;
-    pqInsert(pq, 0, 0);
-    while(pq->size) {
-        pair cur = pop(pq);
-        if(n[cur.nodeNum].toCost!=INF) continue;
-        n[cur.nodeNum].toCost=cur.cost;
-        printf(" %d", cur.nodeNum+1);
-        sum += cur.cost;
-
-        list *l = n[cur.nodeNum].list;
-        while(l->next) {
-            l = l->next;
-            node *next = getAnotherNode(&n[cur.nodeNum], l->e);
-            if(next->toCost==INF) {
-                pqInsert(pq, next->nodeNum, l->e->cost);
-            }
-        }
-    }
-    printf("\n%d", sum);
-
-    // Free
-    free(pq->heap);
-    free(pq);
-    free(e);
-    for(int i=0;i<N;i++) freeList(n[i].list);
+    // free
+    for(int i=0;i<N;i++) freeList(n[i].l);
     free(n);
+    free(e);
 }
+/*
+Input
+5 7
+1 2 1
+1 4 2
+1 5 4
+2 5 7
+4 5 3
+3 5 5
+2 3 6
+
+Output
+ 1 2 4 5 3
+11
+*/
